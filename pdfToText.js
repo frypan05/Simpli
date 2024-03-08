f069a22eea8ebac0ce3975255ca04b4f0eb7e202
frypan05 <daksharma5804@gmail.com>
2024-03-08 21:39:48 +0530

    Updated with pdf parsing

---
+++ pdfToText.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const fileUpload = require ("express-fileupload");
+const pdfParse  = require("pdf-parse");
+
+const app = express();
+
+
+app.get("/", function (req, res) {
+    res.send("Welcome to the PDF text extractor!");
+  });
+
+app.use("/", express.static("public"));
+app.use(fileUpload());
+
+app.post("extract-text", (req,res)=> {
+    if (!req.files &&  !req.files.pdfFile){
+        res.status(400);
+        res.end();
+    }
+    pdfParse(req.files.pdfFile).then(result => {
+        res.send(result.text)
+    });
+    
+});
+app.listen(3000);
